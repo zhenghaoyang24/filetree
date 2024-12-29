cf4cce497ac0305270593a41dd8a269d0d6d7e75
zhenghaoyang24 <zhenghaoyang24@foxmail.com>
2024-12-29 22:03:01 +0800

    dev:nav

---
+++ src/components/NavBar.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+
+
+
+
+</script>
+
+<template>
+  <nav class="navbar">
+    <div>filetree</div>
+  </nav>
+
+</template>
+
+<style scoped lang="less">
+.navbar {
+  padding: 0 10px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  color: black;
+  user-select: none;
+  height: @nav-height;
+  background-color: var(--nav-bg-color);
+}
+
+</style>
